a56640cd1606dc7be2b1fb87096677263a384680
midasinc <midas.inc@gmail.com>
2018-11-16 11:56:37 +0200

    Created function to get page from server

--- weatherapp.py
+++ weatherapp.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 """
 Weather app project.
-This is good project.
 """
+
+from urllib.request import urlopen, Request
+
+accu_url = "https://www.accuweather.com/uk/ua/dnipro/322722/daily-weather-forecast/322722?day=1"
+
+# getting page from server
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux Mint; Linux x86_64;)'}
+accu_request = Request(accu_url, headers=headers)
+accu_page = urlopen(accu_request).read()
+
+print(accu_page)
