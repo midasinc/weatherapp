c280103e838ed1136859408f78714f2f9fe8100a
midasinc <midas.inc@gmail.com>
2018-11-28 17:49:23 +0200

    Added weather from site RP5. Added ability to handle more than 1 tag (for site RP5).

--- weatherapp.py
+++ weatherapp.py
@@ -5,10 +5,17 @@ Weather app project.
 import html
 from urllib.request import urlopen, Request
 
-# URL for current weather in the city of Dnipro on Accuweather site
+# URL and tags for current weather in the city of Dnipro on Accuweather site
 ACCU_URL = "https://www.accuweather.com/uk/ua/dnipro/322722/weather-forecast/322722"
 ACCU_TAGS = ('<span class="large-temp">', '<span class="cond">')
 
+# URL and tags for current weather in the city of Dnipro on RP5 site
+RP5_URL = (
+    'http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%94%D0'
+    '%BD%D1%96%D0%BF%D1%80%D1%96_(%D0%94%D0%BD%D1%96%D0%BF%D1%80%D0%BE'
+    '%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D1%83)')
+RP5_TAGS = ('<span class="t_0" style="display: block;">', 
+            ('<div class="ArchiveInfo">', '°F</span>, '))
 
 def get_request_headers():
     return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64)'}
@@ -26,14 +33,19 @@ def get_page_source(url):
 def get_tag_content(page_content, tag):
     """ Search for relevant information in the content page
     """
-
-    tag_index = page_content.find(tag)
-    tag_size = len(tag)
+    if type(tag) == tuple:
+        tag_index = page_content.find(tag[1], page_content.find(tag[0]))
+        tag_size = len(tag[1])
+        stop = ','
+    else:
+        tag_index = page_content.find(tag)
+        tag_size = len(tag)
+        stop = '<'
     value_start = tag_index + tag_size
 
     content = ''
     for c in page_content[value_start:]:
-        if c != '<':
+        if c != stop:
             content += c
         else:
             break
@@ -60,7 +72,11 @@ def produce_output(provider_name, temp, condition):
 def main():
     """ Main entry point.
     """
-    weather_sites = {"AccuWeather": (ACCU_URL, ACCU_TAGS)}
+    weather_sites = {
+        "AccuWeather": (ACCU_URL, ACCU_TAGS),
+        "RP5": (RP5_URL, RP5_TAGS)
+    }
+
     for name in weather_sites:
         url, tags = weather_sites[name]
         content = get_page_source(url)
