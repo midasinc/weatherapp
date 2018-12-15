183c70486776ac2b0a3d55c3669289505ce187d1
midasinc <midas.inc@gmail.com>
2018-12-15 14:33:37 +0200

    Getting weather  Accu allocated in separate function

--- weatherapp.py
+++ weatherapp.py
@@ -33,18 +33,18 @@ def get_page_source(url):
     return page_source.decode('utf-8')
 
 
-def get_weather_info(page_content):
-    """ Obtaining the required data from the received content and 
-        specify the tags
+def get_weather_info(command, page_content):
+    """ Receiving the current weather data
     """
-    city_page = BeautifulSoup(page_content, "lxml")
-    
-    current_day_section = city_page.find('li', class_='day current first cl')
+    def get_weather_info_accu(city_page):
+        """ TODO: Получение данных о текущей погоде для AccuWeather    
+        """
+        
+        current_day_section = city_page.find('li', class_='day current first cl')
+        if current_day_section == None:
+            current_day_section = city_page.find('li', class_='night current first cl')
 
-    weather_info = {}
-    if current_day_section == None:
-        current_day_section = city_page.find('li', class_='night current first cl')
-    else:
+        weather_info_accu = {}
         current_day_url = current_day_section.find('a').attrs['href']
         if current_day_url:
             current_day_page = get_page_source(current_day_url)
@@ -55,20 +55,27 @@ def get_weather_info(page_content):
                     current_day.find('div', attrs={'id': 'detail-now'})
                 condition = weather_details.find('span', class_='cond')
                 if condition:
-                    weather_info['cond'] = condition.text
+                    weather_info_accu['cond'] = condition.text
                 temp = weather_details.find('span', class_='large-temp')
                 if temp:
-                    weather_info['temp'] = temp.text
+                    weather_info_accu['temp'] = temp.text
                 feal_temp = weather_details.find('span', class_='small-temp')
                 if feal_temp:
-                    weather_info['feal_temp'] = feal_temp.text
+                    weather_info_accu['feal_temp'] = feal_temp.text
 
                 wind_info = weather_details.find_all('li', class_='wind')
                 if wind_info:
-                    weather_info['wind'] = \
+                    weather_info_accu['wind'] = \
                         ' '.join(map(lambda t: t.text.strip(), wind_info))
-    return weather_info
+        return weather_info_accu
 
+
+    city_page = BeautifulSoup(page_content, "lxml")
+    weather_info = {}
+    if command == 'accu':
+        weather_info = get_weather_info_accu(city_page)
+    
+    return weather_info
 
 def produce_output(name, info):
     """ Output of the received data
@@ -88,7 +95,7 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('command', help='Service name', nargs=1)
     params = parser.parse_args(argv)
-
+    
     weather_sites = {
         "AccuWeather": (ACCU_URL, ACCU_TAGS),
         "RP5": (RP5_URL, RP5_TAGS)
@@ -102,11 +109,10 @@ def main(argv):
         else:
             print("Unknown command provided!")
             sys.exit(1)
-
     for name in weather_sites:
         url, tags = weather_sites[name]
         content = get_page_source(url)
-        produce_output(name, get_weather_info(content))
+        produce_output(name, get_weather_info(command, content))
 
 
 if __name__ == '__main__':
