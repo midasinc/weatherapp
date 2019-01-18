f83a2977f78a7afcba7eaddf10dac6f083a8434e
midasinc <midas.inc@gmail.com>
2019-01-18 11:55:41 +0200

    Deleted constants from the weatherapp.py module

--- weatherapp.py
+++ weatherapp.py
@@ -31,34 +31,6 @@ from urllib.parse import quote
 import requests
 
 
-PROVIDER_NAME = {'accu': 'AccuWeather', 'rp5': 'RP5'}
-CONFIG_FILE = 'weatherapp.ini'
-DEFAULT_NAME = 'Дніпро'
-
-# AccuWeather section
-ACCU_URL = (
-    "https://www.accuweather.com/uk/ua/dnipro/322722/weather-forecast/322722")
-ACCU_DEFAULT_URL = (
-    'https://www.accuweather.com/uk/ua/dnipro/322722/weather-forecast/322722')
-ACCU_BROWSE_LOCATIONS = 'https://www.accuweather.com/uk/browse-locations'
-
-# RP5 section
-RP5_URL = (
-    'http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%94%D0%BD%D1'
-    '%96%D0%BF%D1%80%D1%96_%28%D0%94%D0%BD%D1%96%D0%BF%D1%80%D0%BE%D0%BF%D0%B5'
-    '%D1%82%D1%80%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D1%83%29')
-RP5_DEFAULT_URL = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_'
-                   '%D0%9A%D0%B8%D1%94%D0%B2%D1%96')
-RP5_BROWSE_LOCATIONS = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_'
-                        '%D0%B2_%D1%81%D0%B2%D1%96%D1%82%D1%96')
-ADD_URL = 'http://rp5.ua/'
-
-
-# Cache constants
-CACHE_DIR = '.wappcache'
-CACHE_TIME = 10
-
-
 def get_request_headers():
     return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64)'}
 
