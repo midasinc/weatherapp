04530ad71f45470e9ca7b68c5edb70156ecbeeba
midasinc <midas.inc@gmail.com>
2019-01-29 11:54:40 +0200

    Added methods to the ProviderManager class of the providermanager.py module:
        - _load_providers()
        - add()
        - get()
        - __len__()

--- providermanager.py
+++ providermanager.py
@@ -1,3 +1,6 @@
+from providers import AccuWeatherProvider, RP5Provider
+
+
 class ProviderManager:
 
     """ Discovers registered providers and loads them.
@@ -5,3 +8,25 @@ class ProviderManager:
 
     def __init__(self):
         self._providers = {}
+        self._load_providers()
+
+    def _load_providers(self):
+        """ Loads all existing providers.
+        """
+
+        for provider in [AccuWeatherProvider, RP5Provider]:
+            self.add(provider.name, provider)
+
+    def add(self, name, provider):
+        """ Add new provider by name.
+        """
+        self._providers[name] = provider
+
+    def get(self, name):
+        """ Get provider by name.
+        """
+
+        return self._providers.get(name, None)
+
+    def __len__(self, name):
+        return len(self._providers)
