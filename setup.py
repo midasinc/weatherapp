f1496c4db8174fe9d08c00c6d19dc7bae31f9ec2
midasinc <midas.inc@gmail.com>
2019-03-06 17:19:26 +0200

    Added installation script.
       - Meta-information about the package is defined
       - All packages are included
       - Added installation requirements
       - Added entry point for group 'console_scripts'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="weatherapp.core",
+    version="0.1.0",
+    author="Midas Inc",
+    description="A simple cli weather aggregator",
+    long_descriptoin="",
+    packages=find_namespace_packages(),
+    entry_points={
+        'console_scripts': 'wfapp=weatherapp.core.app:main'
+    },
+    install_requires=[
+        'requests',
+        'bs4',
+    ]
+)
