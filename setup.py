d21b310437ffc49f87cd6bd3d908ecf7cf73278c
midasinc <midas.inc@gmail.com>
2019-03-07 14:44:04 +0200

    Fixed and updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="weatherapp.core",
     version="0.1.0",
-    author="Midas Inc",
+    author="Viacheslav Liash",
+    author_email="author@example.com",
     description="A simple cli weather aggregator",
     long_descriptoin="",
+    url = "https://github.com/midasinc/weatherapp.core",
     packages=find_namespace_packages(),
     entry_points={
         'console_scripts': 'wfapp=weatherapp.core.app:main'
@@ -14,5 +16,10 @@ setup(
         'requests',
         'bs4',
         'lxml',
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
