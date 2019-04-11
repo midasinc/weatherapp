38e37848d9a1d3e8b2b46b5d000775639c7249d8
midasinc <midas.inc@gmail.com>
2019-04-11 17:11:16 +0300

    Implemented unittest for App class

--- weatherapp/core/app.py
+++ weatherapp/core/app.py
@@ -29,7 +29,8 @@ class App:
         self.commandmanager = CommandManager()
         self.formatters = self._load_formatters()
          
-    def _arg_parse(self):
+    @staticmethod
+    def _arg_parse():
         """ Initialize argument parser
         """
 
---
+++ weatherapp/core/tests/unit/test_app.py
@@ -0,0 +1,43 @@
+""" Unit tests for App class """
+
+import unittest
+import argparse
+
+from weatherapp.core.app import App
+
+
+class AppTestCase(unittest.TestCase):
+
+    """ Test application class methods.
+    """
+
+    def setUp(self):
+        self.parser = App._arg_parse()
+
+    def test_arg_parser(self):
+        """ Test application argument parser creation.
+        """
+
+        self.assertIsInstance(self.parser, argparse.ArgumentParser)
+
+    def test_arg_parser_default_values(self):
+        """ Test application argument parser default values.
+        """
+
+        parsed_args = self.parser.parse_args([])
+        self.assertIsNone(parsed_args.command)
+        self.assertFalse(parsed_args.debug)
+        self.assertEqual(parsed_args.formatter, 'table')
+
+    def test_arg_parser_arg(self):
+        """ Test application argument parser.
+        """
+
+        parsed_args = self.parser.parse_args(['accu', '--debug', '--refresh',
+                                              '-v'])
+
+        self.assertEqual(parsed_args.command, 'accu')
+        self.assertTrue(parsed_args.debug)
+        self.assertEqual(parsed_args.formatter, 'table')
+        self.assertTrue(parsed_args.refresh)
+        self.assertEqual(parsed_args.verbose_level, 1)
